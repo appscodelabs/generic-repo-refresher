72b746223bf2c09280561db60f17958747aca468
Tamal Saha <tamal@appscode.com>
2022-12-09 02:04:46 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -19,122 +19,12 @@ refresh() {
     cd $(ls -b1)
     git checkout -b $PR_BRANCH
 
-
-
-    sed -i 's/busybox:1.31.1/busybox:latest/g' Makefile
-    # sed -i 's/alpine:3.11/alpine:latest/g' Makefile
-    sed -i 's/alpine:3.10/alpine:latest/g' Makefile
-    # sed -i 's/debian:stretch/debian:buster/g' Makefile
-    sed -i 's/debian:buster/debian:bullseye/g' Makefile
-    # sed -i 's/debian:stretch/debian:bullseye/g' Dockerfile.in || true
-    # sed -i 's/debian:stretch/debian:bullseye/g' Dockerfile.dbg || true
-    sed -i 's/debian:buster/debian:bullseye/g' Dockerfile.in || true
-    sed -i 's/debian:buster/debian:bullseye/g' Dockerfile.dbg || true
-    # sed -i 's/chart-testing:v3.0.0/chart-testing:v3.4.0/g' Makefile
-    # sed -i 's/chart-testing:v3.4.0/chart-testing:v3.5.1/g' Makefile
-    sed -i 's/chart-testing:v3.5.0/chart-testing:v3.5.1/g' Makefile
-    # sed -i 's/?=\ 1.15/?=\ 1.16/g' Makefile
-    # sed -i 's/?=\ 1.16/?=\ 1.17/g' Makefile
-    # sed -i 's/?=\ 1.17/?=\ 1.18/g' Makefile
-    sed -i 's/?=\ 1.18/?=\ 1.19/g' Makefile
-    sed -i 's|appscode/golang-dev:1.16|appscode/golang-dev:1.19|g' Makefile
-    sed -i 's|appscode/golang-dev:1.17|appscode/golang-dev:1.19|g' Makefile
-    sed -i 's|appscode/golang-dev:1.18|appscode/golang-dev:1.19|g' Makefile
-    sed -i 's|appscode/gengo:release-1.24|appscode/gengo:release-1.25|g' Makefile
-    sed -i 's|verify-modules verify-gen|verify-gen verify-modules|g' Makefile
-
-    # https://github.com/GoogleContainerTools/distroless/pull/335
-    # https://github.com/GoogleContainerTools/distroless/blob/70f4a32ab305eec38d9d1c6e5bce2e3a9b92f877/base/BUILD#L10
-    # sed -i '/nobody:nobody/d' Dockerfile
-    # sed -i 's|USER 65535:65535|USER nobody|g' Dockerfile
-    # sed -i '/nobody:nobody/d' Dockerfile.*
-    # sed -i 's|USER 65535:65535|USER nobody|g' Dockerfile.*
-
-    # DO NOT use nonroot base imge
-    # This causes https://github.com/tektoncd/triggers/issues/781
-    # or, https://githubmemory.com/repo/confluentinc/kafka-images/issues/76
-    # sed -i 's|gcr.io/distroless/static-debian10:nonroot|gcr.io/distroless/static:nonroot|g' Makefile
-    # sed -i 's|gcr.io/distroless/static-debian10|gcr.io/distroless/static:nonroot|g' Makefile
-
-    sed -i 's|gcr.io/distroless/static:nonroot|gcr.io/distroless/static-debian10|g' Makefile
-    sed -i 's|gcr.io/distroless/static-debian10|gcr.io/distroless/static-debian11|g' Makefile
-    sed -i 's|gcr.io/distroless/base-debian10|gcr.io/distroless/base-debian11|g' Makefile
-
-    make fmt || true
-    rm -rf hack/kubernetes/storageclass
-    if test -f "hack/kubernetes/kind.yaml"; then
-        cp $GITHUB_WORKSPACE/kind.yaml hack/kubernetes/kind.yaml
+    if test -d .github/workflows/; then
+        cd .github/workflows/
+        sed -i 's|ubuntu-latest|ubuntu-20.04|g' *
+        cd ../..
     fi
 
-    $GITHUB_WORKSPACE/hack/scripts/docker_buildx_fixer.py $(pwd)
-    # $GITHUB_WORKSPACE/hack/scripts/ci_concurrency_fixer.py $(pwd)
-    # $GITHUB_WORKSPACE/hack/scripts/ci_concurrency_fixer.py $(pwd)
-
-    # if grep -q "Apache" hack/scripts/update-release-tracker.sh &> /dev/null; then
-    #     cp $GITHUB_WORKSPACE/hack/scripts/update-release-tracker/apache.sh hack/scripts/update-release-tracker.sh
-    # fi
-    # if grep -q "AppsCode-Community" hack/scripts/update-release-tracker.sh &> /dev/null; then
-    #     cp $GITHUB_WORKSPACE/hack/scripts/update-release-tracker/community.sh hack/scripts/update-release-tracker.sh
-    # fi
-    # if grep -q "AppsCode-Free-Trial" hack/scripts/update-release-tracker.sh &> /dev/null; then
-    #     cp $GITHUB_WORKSPACE/hack/scripts/update-release-tracker/enterprise.sh hack/scripts/update-release-tracker.sh
-    # fi
-
-    pushd .github/workflows/ && {
-        # hugo
-        sed -i 's|0.99.1|0.100.2|g' *
-
-        # update engineerd/setup-kind
-        sed -i 's|jetstack/cert-manager/releases/download/v1.5.3/|cert-manager/cert-manager/releases/download/v1.9.1/|g' *
-
-        sed -i 's|engineerd/setup-kind@v0.4.0|engineerd/setup-kind@v0.5.0|g' *
-        # KIND
-        sed -i 's|version: v0.14.0|version: v0.16.0|g' *
-        # sed -i 's|\[v1.16.15, v1.17.17, v1.18.15, v1.19.7, v1.20.2, v1.21.1\]|\[v1.16.15, v1.17.17, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.0\]|g' *
-        # sed -i 's|(v1.16.15 v1.18.15 v1.21.1)|(v1.16.15 v1.18.19 v1.20.7 v1.22.0)|g' *
-        # sed -i 's|\[v1.16.15, v1.17.17, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.0\]|\[v1.16.15, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.3\]|g' *
-        # sed -i 's|(v1.16.15 v1.18.19 v1.20.7 v1.22.0)|(v1.19.11 v1.21.2 v1.23.3)|g' *
-        # sed -i 's|\[v1.16.15, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.0\]|\[v1.16.15, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.3\]|g' *
-        # sed -i 's|(v1.19.11 v1.21.2 v1.23.0)|(v1.19.11 v1.21.2 v1.23.3)|g' *
-
-        #sed -i 's|\[v1.16.15, v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.3\]|\[v1.18.20, v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0\]|g' *
-        #sed -i 's|\[v1.18.19, v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.3\]|\[v1.18.20, v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0\]|g' *
-        sed -i 's|\[v1.18.20, v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0\]|\[v1.18.20, v1.19.16, v1.20.15, v1.21.14, v1.22.15, v1.23.12, v1.24.6, v1.25.2\]|g' *
-        sed -i 's|\[v1.18.20, v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0, , v1.25.0\]|\[v1.18.20, v1.19.16, v1.20.15, v1.21.14, v1.22.15, v1.23.12, v1.24.6, v1.25.2\]|g' *
-
-        # sed -i 's|\[v1.19.11, v1.20.7, v1.21.2, v1.22.4, v1.23.3\]|\[v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0\]|g' *
-        sed -i 's|\[v1.19.16, v1.20.15, v1.21.12, v1.22.9, v1.23.6, v1.24.0\]|\[v1.18.20, v1.19.16, v1.20.15, v1.21.14, v1.22.15, v1.23.12, v1.24.6, v1.25.2\]|g' *
-
-        # sed -i 's|\[v1.14.10, v1.16.9, v1.18.8, v1.19.1\]|\[v1.18.20, v1.20.15, v1.22.9, v1.24.0\]|g' *
-        sed -i 's|\[v1.18.20, v1.20.15, v1.22.9, v1.24.0\]|\[v1.18.20, v1.20.15, v1.22.15, v1.24.6, v1.25.2\]|g' *
-
-        # sed -i 's|(v1.19.11 v1.21.2 v1.23.3)|(v1.18.20 v1.20.15 v1.22.9 v1.24.0)|g' *
-        sed -i 's|(v1.18.20 v1.20.15 v1.22.9 v1.24.0)|(v1.18.20 v1.20.15 v1.22.15 v1.24.6 v1.25.2)|g' *
-
-        # update GO
-        sed -i 's/Go\ 1.18/Go\ 1.19/g' *
-        sed -i 's/go-version:\ 1.18/go-version:\ 1.19/g' *
-        sed -i 's/go-version:\ ^1.18/go-version:\ ^1.19/g' *
-        sed -i "s/node-version:\ '14'/node-version:\ '16'/g" *
-        sed -i "s/node-version:\ 14.x/node-version:\ '16'/g" *
-        sed -i 's|/gh-tools/releases/download/v0.2.12/|/gh-tools/releases/download/v0.2.13/|g' *
-        sed -i 's|/release-automaton/releases/download/v0.0.36/|/release-automaton/releases/download/v0.0.37/|g' *
-        sed -i 's|/hugo-tools/releases/download/v0.2.21/|/hugo-tools/releases/download/v0.2.23/|g' *
-        popd
-    }
-    [ -f go.mod ] && {
-        sed -i 's|ioutil.ReadFile|os.ReadFile|g' `grep 'ioutil.ReadFile' -rl *`
-        sed -i 's|ioutil.WriteFile|os.WriteFile|g' `grep 'ioutil.WriteFile' -rl *`
-        sed -i 's|ioutil.ReadAll|io.ReadAll|g' `grep 'ioutil.ReadAll' -rl *`
-        sed -i 's|ioutil.TempDir|os.MkdirTemp|g' `grep 'ioutil.TempDir' -rl *`
-        sed -i 's|ioutil.TempFile|os.CreateTemp|g' `grep 'ioutil.TempFile' -rl *`
-
-        # go get github.com/modern-go/reflect2@v1.0.2
-        # go get github.com/json-iterator/go@v1.1.12
-        go mod tidy
-        go mod vendor
-    }
-    # make gen || true
     make fmt || true
     [ -z "$2" ] || (
         echo "$2"
